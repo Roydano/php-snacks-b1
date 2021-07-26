de3ce31cf3f3120c405632f396ba2c5e48028eae
Roydano <roy.dano90@gmail.com>
2021-07-26 16:28:58 +0200

    add snack2

--- snack1.php
+++ snack1.php
@@ -48,7 +48,7 @@ var_dump($basketMatchLength);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snack b1 - php</title>
+    <title>Snack1 - b1</title>
 </head>
 <body>
 
